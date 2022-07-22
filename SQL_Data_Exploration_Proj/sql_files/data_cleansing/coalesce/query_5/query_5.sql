aed337fe117709ff5753bcaad8da2d9dc08a6ac2
wahyunh10 <66968995+wahyunh10@users.noreply.github.com>
2022-07-22 13:54:02 +0700

    update query

---
+++ SQL_Data_Exploration_Proj/sql_files/data_cleansing/coalesce/query_5/query_5.sql
@@ -0,0 +1,13 @@
+/*Query 5 - without the WHERE - number of ids - new table after appying coalesce to all the required columns*/
+SELECT *,
+COALESCE(a.ï»¿id ,a.ï»¿id) AS modified_id,
+COALESCE(o.account_id,a.ï»¿id) AS modified_acc,
+COALESCE(o.standard_qty,0) AS modified_st_qty,
+COALESCE(o.poster_qty,0) AS modified_pos_qty,
+COALESCE(o.gloss_qty,0) AS modified_gloss_qty,
+COALESCE(o.standard_amt_usd,0) AS modified_std_amt,
+COALESCE(o.gloss_amt_usd,0) AS modified_glo_amt,
+COALESCE(o.poster_amt_usd,0) AS modified_pos_amt
+FROM accounts AS a
+LEFT JOIN orders2 AS o
+ON a.ï»¿id = o.account_id;
